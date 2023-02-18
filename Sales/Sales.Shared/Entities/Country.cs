45a51850d3cd0aef24b6a96054480f5febfe879b
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-02-18 16:06:52 -0500

    creation of base architecture and database model

---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; } // Codigo del país
+
+        // DATANOTATION
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100,ErrorMessage = "El campo {0} no puede tener más de {1} caracteres ")]
+        public string Name { get; set; } = null!;
+    }
+}
