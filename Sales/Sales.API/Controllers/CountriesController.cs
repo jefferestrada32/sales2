e2ee769aeebed0ff2745734420d49cc492ee9817
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-02-19 12:54:07 -0500

    Begin countries API controler

---
+++ Sales/Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult>  GetAsync() 
+        { 
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        // ActionResult es el resultado de la petición
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();  
+            return Ok(country);
+
+        }
+    }
+}
