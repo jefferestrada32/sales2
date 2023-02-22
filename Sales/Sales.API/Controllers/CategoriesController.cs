7fa120b31353c6f66fbb80ae1cfd5ca46b1e912d
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-02-21 21:49:12 -0500

    Create category API

---
+++ Sales/Sales.API/Controllers/CategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        // ActionResult es el resultado de la petición
+        public async Task<ActionResult> PostAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpPut]
+        // ActionResult es el resultado de la petición
+        public async Task<ActionResult> PutAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Sales/Sales.API/Migrations/20230222020245_addCategory.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries");
+
+            migrationBuilder.RenameTable(
+                name: "Countries",
+                newName: "Country");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Countries_Name",
+                table: "Country",
+                newName: "IX_Country_Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Country",
+                table: "Country",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name",
+                table: "Category",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Country",
+                table: "Country");
+
+            migrationBuilder.RenameTable(
+                name: "Country",
+                newName: "Countries");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Country_Name",
+                table: "Countries",
+                newName: "IX_Countries_Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries",
+                column: "Id");
+        }
+    }
+}
