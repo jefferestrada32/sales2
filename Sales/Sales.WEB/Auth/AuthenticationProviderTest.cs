495244bc3312c970c84a1348a98649480b5d991a
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-03-31 23:43:06 -0500

    Secure of fronend

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var jefferUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Jefferson"),
+                new Claim("LastName", "Estrada"),
+                new Claim(ClaimTypes.Name, "jestrada@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+        authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+}
