7cc70450fa25ffcf591b3c0e2d5c7fd9fb5eb444
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-04-22 16:01:29 -0500

    new entity, new dto

--- Sales/Sales.API/Data/DataContext.cs
+++ Sales/Sales.API/Data/DataContext.cs
@@ -13,6 +13,12 @@ namespace Sales.API.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
         public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,6 +27,7 @@ namespace Sales.API.Data
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("CountryId","Name").IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<City>().HasIndex("StateId","Name").IsUnique();
 
         }
